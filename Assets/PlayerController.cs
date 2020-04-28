4431d99fe030f1974711645cd9404617518b89cd
trinplaytika <minhn@playtika.com>
2020-04-28 05:27:54 -0400

    Init Project Started setting walk, run, crouch animations

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator = null;
+    bool crouch = false;
+    
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 inputMovement = value.Get<Vector2>();
+        animator.SetFloat("Horizontal", inputMovement.x);
+        animator.SetFloat("Vertical", inputMovement.y);
+    }
+
+    public void OnCrouch()
+    {
+        if(crouch)
+        {
+            crouch = false;
+        }
+        else
+        {
+            crouch = true;
+        }
+        animator.SetBool("Crouch", crouch);
+    }
+
+    public void OnRun(InputValue value)
+    {
+        float speed = value.Get<float>() + 1f;
+        
+        animator.SetFloat("Speed", speed);
+    }
+
+    public void OnDodge()
+    {
+        animator.SetTrigger("Dodge");
+    }
+}
