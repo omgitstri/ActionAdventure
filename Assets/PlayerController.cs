fed3d70a018bbf60e00530c0d38233daec67682d
trinplaytika <minhn@playtika.com>
2020-04-29 21:28:57 -0400

    Setup on screen UI, test apk build: success

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,29 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
     Animator animator = null;
     bool crouch = false;
-    
-
+    public CinemachineFreeLook cam = null;
+    public PlayerInput playerInput = null;
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        transform.rotation = Quaternion.identity;
+    }
 
     public void OnMove(InputValue value)
     {
         Vector2 inputMovement = value.Get<Vector2>();
-        animator.SetFloat("Horizontal", inputMovement.x);
-        animator.SetFloat("Vertical", inputMovement.y);
+        //animator.SetFloat("Speed", inputMovement.magnitude + 1f);
+        animator.SetFloat("Horizontal", inputMovement.x * 2);
+        animator.SetFloat("Vertical", inputMovement.y * 2);
     }
 
     public void OnCrouch()
     {
-        if(crouch)
+        if (crouch)
         {
             crouch = false;
         }
@@ -35,15 +41,16 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("Crouch", crouch);
     }
 
-    public void OnRun(InputValue value)
-    {
-        float speed = value.Get<float>() + 1f;
-        
-        animator.SetFloat("Speed", speed);
-    }
-
     public void OnDodge()
     {
         animator.SetTrigger("Dodge");
+    }
+
+    public void OnCameraLook(InputValue value)
+    {
+        Vector2 inputMovement = value.Get<Vector2>();
+
+        cam.m_XAxis.m_InputAxisValue = inputMovement.x;
+        cam.m_YAxis.m_InputAxisValue = inputMovement.y;
     }
 }
---
+++ Assets/testcomponent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testcomponent : MonoBehaviour
+{
+    RectTransform rectTransform = null;
+    public float xMin = 0;
+    public float xMax = 0;
+    public float yMin = 0;
+    public float yMax = 0;
+
+    public Vector3 offset = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        var screenRect = new Rect(0f, 0f, Camera.main.pixelWidth, Camera.main.pixelHeight);
+        var min = (Vector2)rectTransform.position - (Vector2.Scale(rectTransform.rect.size, rectTransform.lossyScale) * 0.5f);
+        var max = (Vector2)rectTransform.position + (Vector2.Scale(rectTransform.rect.size, rectTransform.lossyScale) * 0.5f);
+        Debug.Log(max);
+
+
+
+        if (min.x < 0)
+            rectTransform.position -= new Vector3(min.x, 0, 0);
+        else if (max.x > screenRect.width)
+            rectTransform.position -= new Vector3(max.x - screenRect.width, 0, 0);
+
+
+
+        //foreach (var corner in pointerCorners)
+        //{
+        //    // screen does not contain pointer corner
+        //    if (!screenRect.Contains(corner))
+        //    {
+        //        // move element to the left a bit
+        //        rectTransform.localPosition += new Vector3(-50, 0, 0);
+        //        break;
+        //    }
+        //}
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Vector3[] v = new Vector3[4];
+        //rectTransform.GetWorldCorners(v);
+
+        ////Bottom Left
+        //Debug.Log(v[0]);
+        //if (Camera.main.ScreenToViewportPoint(img.rect.min).x < 0)
+        //    Debug.Log("out of screen");
+    }
+}
