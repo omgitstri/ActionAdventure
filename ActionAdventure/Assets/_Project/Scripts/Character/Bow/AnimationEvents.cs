56f0a60ba696a8ed439e61305cee33106fdb9d5e
Minh-Tri Nguyen <omgitstri@hotmail.com>
2020-06-21 02:01:59 -0400

    merged prototype

--- ActionAdventure/Assets/_Project/Scripts/Character/Bow/AnimationEvents.cs
+++ ActionAdventure/Assets/_Project/Scripts/Character/Bow/AnimationEvents.cs
@@ -7,6 +7,8 @@ public class AnimationEvents : MonoBehaviour
 {
     [SerializeField] UnityEvent _equipBow = null;
     [SerializeField] UnityEvent _disarmBow = null;
+    [SerializeField] Transform _bow = null;
+    [SerializeField] GameObject _arrow = null;
 
     public void EquipBow()
     {
@@ -16,5 +18,25 @@ public class AnimationEvents : MonoBehaviour
     public void DisarmBow()
     {
         _disarmBow.Invoke();
+    }
+
+    public void Shoot()
+    {
+        var go = Instantiate(_arrow);
+        go.transform.SetParent(_bow);
+
+        go.transform.localScale = Vector3.one;
+        go.transform.localRotation = Quaternion.identity;
+        go.transform.localPosition = Vector3.zero;
+        go.GetComponent<Rigidbody>().AddRelativeForce(go.transform.up * 30, ForceMode.Impulse);
+
+        go.transform.SetParent(null);
+
+        _bow.gameObject.SetActive(false);
+    }
+
+    public void SetArrow()
+    {
+        _bow.gameObject.SetActive(true);
     }
 }
--- ActionAdventure/Assets/_Project/Scripts/EnemyBehaviour.cs
+++ ActionAdventure/Assets/_Project/Scripts/EnemyBehaviour.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 [ExecuteAlways]
 public class EnemyBehaviour : MonoBehaviour
 {
-    [Range(0,1)]
+    [Range(0, 1)]
     [SerializeField] private float a = 0f;
     [SerializeField] private bool _dodge = false;
     [SerializeField] private bool _damage = false;
 
     [SerializeField] private Transform hitDirection = null;
+    Vector3 collisionPoint = Vector3.zero;
+
+    [SerializeField] CinemachineFreeLook overshoulder;
 
     void Update()
     {
+        //if(!_dodge && !_damage)
+        //{
+        //    foreach (var item in transform.GetComponentsInChildren<EnemyVisual>(true))
+        //    {
+        //        item.Start();
+        //    }
+        //}
+
         if (_dodge)
         {
             foreach (var item in transform.GetComponentsInChildren<EnemyVisual>())
             {
                 item.transform.localPosition = Vector3.LerpUnclamped(item.initPos, item.initPos + item.random, a);
                 item.transform.localScale = Vector3.Lerp(Vector3.one * 0.125f, Vector3.zero, a - 0.5f);
+                //overshoulder.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = Mathf.Lerp(0, 1, a);
             }
         }
 
@@ -27,15 +40,32 @@ public class EnemyBehaviour : MonoBehaviour
         {
             foreach (var item in transform.GetComponentsInChildren<EnemyVisual>())
             {
-                item.transform.position = Vector3.Lerp(item.initPosWorld, (item.initPosWorld - hitDirection.position).normalized * 3 + item.random2, a);
+                //item.transform.position = Vector3.Lerp(item.initPosWorld, (item.initPosWorld - hitDirection.position).normalized * 3 + item.random2, a);
+                item.transform.localPosition = Vector3.Lerp(item.initPos, (item.initPosWorld - collisionPoint).normalized * 3 + item.random2, a);
                 item.transform.localScale = Vector3.Lerp(Vector3.one * 0.125f, Vector3.zero, a - 0.2f);
+
+                overshoulder.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = Mathf.Lerp(2, 0, a);
             }
         }
     }
 
-    private void OnMouseDown()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        //collisionPoint = collision.GetContact(0).point;
+        collisionPoint = collision.transform.position;
+
+
+
+        if (Random.value > 0.25f)
+        {
+            collision.gameObject.SetActive(false);
+            GetComponent<Animator>().SetTrigger("Damage");
+            //collision.gameObject.SetActive(false);
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Dodge");
+        }
     }
 
     public void ResetAll()
@@ -56,5 +86,10 @@ public class EnemyBehaviour : MonoBehaviour
         {
             item.enabled = true;
         }
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
